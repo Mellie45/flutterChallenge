67c8f6d592037b98c13bfdb168f3a62bca9f01f9
Mellie45 <melcolton@icloud.com>
2023-12-01 12:16:21 +0000

    test commit

--- lib/utilities/datetime_object.dart
+++ lib/utilities/datetime_object.dart
@@ -23,9 +23,7 @@ class _DateObjectState extends State<DateObject> {
   String updatedValue() {
     var yearData = now.year;
     var monthData = now.month;
-
     var dayData = now.day;
-
     var timeData = DateFormat.jm().format(DateFormat('hh:mm').parse('${now.hour}:${now.minute}'));
     String mainDate = '${yearData.toString()} ${monthData.toString().padLeft(2, '0')} ${dayData.toString().padLeft(2, '0')}';
     String data = "$mainDate $timeData";
